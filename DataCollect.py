27cc30e9422f5ad70f085c95caaa0dab14ff4fff
Colin McBride <11056834@live.mercer.edu>
2024-09-18 07:22:48 -0400

    test

--- DataCollect.py
+++ DataCollect.py
@@ -1,9 +1,16 @@
 from polygon import RESTClient
+from datetime import datetime
 import datetime as dt
 import pandas as pd
 from APIKey import getPolygonAPIkey
 
-# Returns list of dataframes for each stock from API 
+# Future: 
+# Add csv for each stock passed
+# If CSV exists skip queries
+# Add delays to ensure API does not timeout
+
+# Queries API for stocks in list argument
+# Returns list of dataframes for each stock
 def GetData(stocks):
     rawData = list()
     client = RESTClient(getPolygonAPIkey())
@@ -14,14 +21,18 @@ def GetData(stocks):
                     ticker      = stock,
                     multiplier  = 1,
                     timespan    = 'hour',
-                    from_       = '2023-08-01',
-                    to          = '2023-08-01')))
+                    from_       = datetime(2024, 1, 1, 00, 00, 00),
+                    to          = datetime(2024, 1, 5, 00, 00, 00))))
         
     return rawData
 
-def CheckData():
-    pass
+# Implement in future
+# Returns False
+def CheckData(Stock):
+    return False
 
+# Deletes unused columns and sets timestamps as index
+# Returns list of cleaned dataframes 
 def HandleData(rawData):
     cleanData = list()
     for dataFrame in rawData:
@@ -38,33 +49,10 @@ def main():
     stocks = ["AAPL"]
     rawData = GetData(stocks)
     cleanData = HandleData(rawData)
-    print(cleanData[0])
+
+    for stock in cleanData:
+        if not CheckData(stock):
+            stock.to_csv('test.csv', index = True)
 
 if __name__=="__main__":
     main()
-
-
-
-"""# create client with API key 
-client = RESTClient(getPolygonAPIkey())
-
-def cleanDataSingle(dataFrame):
-    #Convert UNIX timestamps to milliseconds
-    dataFrame['Date'] = dataFrame['timestamp'].apply(
-                          lambda x: pd.to_datetime(x*1000000)) 
-    #Clean up dataframe
-    dataFrame = dataFrame.set_index('Date')
-    dataFrame.drop(columns = ['vwap', 'otc', 'timestamp', 'transactions'], axis=1, inplace=True)
-    return dataFrame
-
-def cleanDataAll(dataFrame, volumeMin):
-    for x in dataFrame.index:
-        if dataFrame.loc[x, "volume"] <  volumeMin:
-            dataFrame.drop(x, inplace = True)
-    dataFrame.drop(columns = ['vwap', 'otc', 'timestamp', 'transactions'], axis=1, inplace=True)
-    dataFrame.reset_index(drop=True, inplace=True)
-    
-    
-data = pd.DataFrame(client.get_grouped_daily_aggs(getConfigurations()[0][1]))
-cleanDataAll(data, getConfigurations()[1][1])
-    """
