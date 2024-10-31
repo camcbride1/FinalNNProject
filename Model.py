4a986b708f9413038f6cbe8975c38a3487aa2a6e
Colin McBride <11056834@live.mercer.edu>
2024-10-31 07:22:16 -0400

    Fix model and create an example of 2024 predictions

--- Model.py
+++ Model.py
@@ -1,9 +1,12 @@
+# %%
 import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import linregress
 
 # Read Dataframe to get data
 df = pd.read_csv('AAPL_2023.csv')
@@ -12,7 +15,7 @@ unchanged_stock_prices = price_column.to_numpy()
 stock_prices = (unchanged_stock_prices - np.min(unchanged_stock_prices)) / (np.max(unchanged_stock_prices) - np.min(unchanged_stock_prices))
 
 # Convert to PyTorch tensors and reshape for LSTM
-sequence_length = 8
+sequence_length = 10
 x_train, y_train = [], []
 
 for i in range(len(stock_prices) - sequence_length):
@@ -23,6 +26,7 @@ for i in range(len(stock_prices) - sequence_length):
 x_train = torch.tensor(x_train, dtype=torch.float32).view(-1, sequence_length, 1)  
 y_train = torch.tensor(y_train, dtype=torch.float32).view(-1, 1)
 
+# %%
 # Define LSTM
 class LSTMModel(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, num_layers):
@@ -43,8 +47,9 @@ class LSTMModel(nn.Module):
         # Fully connected layer (only the output of the last time step)
         out = self.fc(out[:, -1, :])  # Get the last time step output
         return out
-    
-    # Initialize model, loss, and optimizer
+
+# %%
+# Initialize model, loss, and optimizer
 input_size = 1        # Each week has 1 stock price
 hidden_size = 64      # Number of LSTM units
 output_size = 1       # Predicting 1 stock price
@@ -56,6 +61,7 @@ model = LSTMModel(input_size, hidden_size, output_size, num_layers)
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+# %%
 # Training
 for epoch in range(num_epochs):
     model.train()
@@ -70,17 +76,24 @@ for epoch in range(num_epochs):
     if (epoch + 1) % 10 == 0:
         print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')"""
 
+# %%
 # Testing on the training data (for simplicity)
 model.eval()
 predicted = model(x_train).detach().numpy()
 
+# %%
 # Output results to csv
 results = df[['Date', 'open']]
 results = results.set_index('Date')
-nan_array = np.full(8, np.nan)
+nan_array = np.full(10, np.nan)
 nan_array = nan_array.reshape(-1, 1)
 predicted = np.round(predicted * (np.max(unchanged_stock_prices) - np.min(unchanged_stock_prices)) + np.min(unchanged_stock_prices), 2)
 
 predicted = np.concatenate([nan_array, predicted])
 results['predicted'] = predicted
 results.to_csv('results.csv', index = True, na_rep = 'NA')
+
+# %%
+
+
+
